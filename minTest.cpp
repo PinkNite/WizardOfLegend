351d71bc0add0c2162c62311d0866caf27de3008
mindonghwi <min92706@gmail.com>
2019-02-07 12:01:31 +0900

    mintestScene

---
+++ minTest.cpp
@@ -0,0 +1,27 @@
+#include "stdafx.h"
+#include "minTest.h"
+
+MINTESTSCENE::MINTESTSCENE()
+{
+}
+
+MINTESTSCENE::~MINTESTSCENE()
+{
+}
+
+HRESULT MINTESTSCENE::init()
+{
+	return S_OK;
+}
+
+void MINTESTSCENE::update()
+{
+}
+
+void MINTESTSCENE::release()
+{
+}
+
+void MINTESTSCENE::render()
+{
+}
--- minTest.h
+++ minTest.h
@@ -1,1 +1,19 @@
 #pragma once
+
+#include "stdafx.h"
+#include "gameNode.h"
+#include "player.h"
+
+class MINTESTSCENE : public gameNode
+{
+private:
+	PLAYER * _pPlayer;
+public:
+	MINTESTSCENE();
+	~MINTESTSCENE();
+
+	HRESULT init() override;
+	void update() override;
+	void release() override;
+	void render() override;
+};
