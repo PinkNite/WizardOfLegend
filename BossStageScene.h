49ace3bd8c4908c9a112e6a599633b0e1ace5688
airplanet <airplanet.kr@gmail.com>
2019-02-20 20:54:07 +0900

    몬스터 작업중: 타겟 거리 체크

--- BossStageScene.h
+++ BossStageScene.h
@@ -5,16 +5,19 @@
 #include "magicMgr.h"
 #include "camera.h"
 #include "Enemy.h"
+#include "player.h"
+#include "map.h"
 
 class BossStageScene :	public gameNode
 {
 private:
+	MAP*				_pMap;
 	BOSS*				_pBoss;
 	Enemy*				_pEnemy;
+	PLAYER*				_pPlayer;
 	CAMERA*				_pCamera;
 	MAGICMGR*			_pMagicMgr;
-
-	bool _isShow;
+	SKILL_EFFECT_MGR*	_pSkillEffectMgr;
 
 public:
 	BossStageScene();
